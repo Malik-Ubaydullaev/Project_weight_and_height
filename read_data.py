950396b52a4842e505396a7f40f5fd22c3aa262d
Your Name <ubaydullayev@samtuit.uz>
2022-03-12 04:26:40 +0500

    Read_data from csvfile

--- read_data.py
+++ read_data.py
@@ -9,6 +9,15 @@ def read_csv_data(file_path):
         list: data split row
     """
     result_data = []
+    reader = ''
+    with open(file_path, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            result_data.append(row)
+    
+   # print(result_data)
     # WRITE YOUR CODE HERE
     return result_data
-    
+
+file_path = 'data/weight-height.csv'
+read_csv_data(file_path)
