150c67d371130bbd02a8440f947260bf26e06447
Your Name <ubaydullayev@samtuit.uz>
2022-03-12 06:54:04 +0500

    Ozgina ishsi qoldi

--- plot_data.py
+++ plot_data.py
@@ -18,7 +18,15 @@ def show(gender, weight, height):
     h_female = []
     
     #WRITE YOUR CODE HERE
-
+    for x, y, z in zip(gender, weight, height):
+        if x == 0:
+            w_male.append(y)
+            h_male.append(z)
+        if x == 1:
+            w_female.append(y)
+            h_female.append(z)
+        
+    
     plt.show()
 
 data = read_csv_data('data/weight-height.csv')
